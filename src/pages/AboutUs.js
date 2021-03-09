1ec6f39bd10ea5e4cbb01bf8a40417adf49282cc
Kyu Yeon Kim <58721219+kykim102@users.noreply.github.com>
2021-03-10 00:27:43 +1300

    Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 // Page Components
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
-	return <AboutSection />;
+	return (
+		<Fragment>
+			<AboutSection />
+			<ServicesSection />
+		</Fragment>
+	);
 };
 
 export default AboutUs;
