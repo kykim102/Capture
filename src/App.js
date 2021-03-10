72646674499035e3d5b45c64c9672ed741628661
Kyu Yeon Kim <58721219+kykim102@users.noreply.github.com>
2021-03-10 13:02:43 +1300

    Our Work Page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import GlobalStyle from './components/GlobalStyle';
 // Import Pages
 import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+// Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<GlobalStyle />
 			<Nav />
-			<AboutUs />
+			<Switch>
+				<Route exact path="/">
+					<AboutUs />
+				</Route>
+				<Route exact path="/work">
+					<OurWork />
+				</Route>
+				<Route exact path="/contact">
+					<ContactUs />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
